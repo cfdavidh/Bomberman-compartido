92de19299157342e566d78df8349ab6cdbce7f2d
cfdavidh <cfdavidfernandez@yahoo.com>
2024-06-27 04:31:40 -0300

    agregando sprites

--- scripts/jugador/jugador.gd
+++ scripts/jugador/jugador.gd
@@ -1,35 +1,18 @@
 extends CharacterBody2D
 
-#sistema clasico
-'''const SPEED = 300.0
+func _process(delta):
+	if Input.is_action_pressed("ui_up"):
+		move(Vector2.UP)
+	if Input.is_action_pressed("ui_down"):
+		move(Vector2.DOWN)
+	if Input.is_action_pressed("ui_left"):
+		move(Vector2.LEFT)
+	if Input.is_action_pressed("ui_right"):
+		move(Vector2.RIGHT)
 
-func _physics_process(delta):
-	motion_control()
+func move(direction: Vector2):
+	print(direction)
+	
 
-func motion_control() -> void:
-	velocity.x = GLOBAL.get_axis().x * SPEED
-	velocity.y = GLOBAL.get_axis().y * - SPEED
-	move_and_slide()
-
-'''
-
-#sistema tosco movimiento por casilla (no interpreta las coliciones)
-'''var curr_position = [0,0]
-
-func _input(event):
-	if event.is_action_pressed("ui_right"):
-		curr_position[0] += 16
-		get_node("Sprite2D").look_at(self.position + Vector2(1, 0))
-	elif event.is_action_pressed("ui_left"):
-		curr_position[0] -= 16
-		get_node("Sprite2D").look_at(self.position + Vector2(-1, 0))
-	elif event.is_action_pressed("ui_up"):
-		curr_position[1] -= 16
-		get_node("Sprite2D").look_at(self.position + Vector2(0, -1))
-	elif event.is_action_pressed("ui_down"):
-		curr_position[1] += 16
-		get_node("Sprite2D").look_at(self.position + Vector2(0, 1))
-	self.position = Vector2(curr_position[0], curr_position[1])
-'''
 
 
