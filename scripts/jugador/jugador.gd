a446a3180e4fc82d544158b12b1f44bfbbb466f2
cfdavidh <cfdavidfernandez@yahoo.com>
2024-06-30 19:35:22 -0300

    movimiento de personaje

--- scripts/jugador/jugador.gd
+++ scripts/jugador/jugador.gd
@@ -1,18 +1,48 @@
 extends CharacterBody2D
 
-func _process(delta):
-	if Input.is_action_pressed("ui_up"):
-		move(Vector2.UP)
-	if Input.is_action_pressed("ui_down"):
-		move(Vector2.DOWN)
-	if Input.is_action_pressed("ui_left"):
-		move(Vector2.LEFT)
-	if Input.is_action_pressed("ui_right"):
-		move(Vector2.RIGHT)
+@onready var tile_map = $"../Mapa"
+var is_moving = false
+var input_dir
 
-func move(direction: Vector2):
-	print(direction)
+func _physics_process(delta:float) -> void:
+	input_dir = Vector2.ZERO
 	
+	if Input.is_action_pressed("ui_down"):
+		input_dir = Vector2(0, 1)
+		move()
+	elif Input.is_action_pressed("ui_up"):
+		input_dir = Vector2(0, -1)
+		move()
+	elif Input.is_action_pressed("ui_right"):
+		input_dir = Vector2(1, 0)
+		move()
+	elif Input.is_action_pressed("ui_left"):
+		input_dir = Vector2(-1, 0)
+		move()
 
 
+func move():
+	if input_dir:
+		if is_moving == false:
+			is_moving = true
+			
+			#obtiene la posicion del tile actual
+			var current_tile:Vector2i = tile_map.local_to_map(global_position)
+			#obtiene la posicion del tile objetivo (a moverse)
+			var target_tile:Vector2i = Vector2i(
+				current_tile.x + input_dir.x,
+				current_tile.y + input_dir.y,
+			)
+			#obtiene el custom data si es caminable o nó 
+			var tile_data:TileData = tile_map.get_cell_tile_data(0, target_tile)
+			#si no es caminable retorna
+			if tile_data.get_custom_data("walkable") == false:
+				move_false()
+				return
+			
+			var tween = create_tween()
+			tween.tween_property(self, "position", tile_map.map_to_local(target_tile), 1)
+			tween.tween_callback(move_false)
 
+func move_false():
+	is_moving = false
